7ff771d1841abd5f1ba384c9ebf2acdd9b065f7f
Alice Lubic <lubicalice@gmail.com>
2016-07-22 13:56:58 -0400

    it works

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -5,23 +5,47 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText mInput;
+    TextView mWish;
+    Button mAddWish;
+    ListView mWishListView;
+    ArrayList<String> wishlist = new ArrayList<>();
 
-    //TODO: set up all the view and event listeners.
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-  @Override
-  public void onClick(View v) {
-    // TODO: 1. get the text from the input field
-    //       2. animate it in the center of the screen
-    //       3. add it to the list wish
-    //       4. clear the input field
-  }
+        mInput = (EditText) findViewById(R.id.edittext);
+        mWish = (TextView) findViewById(R.id.textview);
+        mAddWish = (Button) findViewById(R.id.button);
+        mWishListView = (ListView) findViewById(R.id.wishlist);
+        mWishListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, wishlist));
+        mAddWish.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String input = mInput.getText().toString();
+        Animation animation = null;
+        animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        mWish.setText(input);
+        mWish.startAnimation(animation);
+        wishlist.add(0, input);
+        mInput.setText("");
+    }
+
+
 }
